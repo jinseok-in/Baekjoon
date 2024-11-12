1ef992090ea780de4d89bfb690c6e0a50ca2f78b
jinseok-in <75413890+jinseok-in@users.noreply.github.com>
2024-11-12 19:17:16 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.03 ms, Memory: 73.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.java
@@ -0,0 +1,16 @@
+class Solution {
+  public int solution(int[] common) {
+    int answer = 0;
+    
+    int a = common[1] - common[0];
+    int b = common[2] - common[1];
+    
+    if (a==b) {
+      answer = common[common.length-1]+a;
+    } else {
+      int c = b / a;
+      answer = common[common.length-1]*c;
+    }
+    return answer;
+  }
+}
