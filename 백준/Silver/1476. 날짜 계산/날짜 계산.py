81dbfc53eb36af20c409301df96488389551d937
jinseok-in <75413890+jinseok-in@users.noreply.github.com>
2025-04-01 17:45:57 +0900

    [Silver V] Title: 날짜 계산, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/1476. 날짜 계산/날짜 계산.py
@@ -0,0 +1,15 @@
+e, s, m = map(int, input().split())
+e_, s_, m_ = 1, 1, 1
+count = 1
+while (e != e_) or (s != s_) or (m != m_) :
+    e_ += 1
+    s_ += 1
+    m_ += 1
+    count += 1
+    if e_ > 15 :
+        e_ = 1
+    if s_ > 28 :
+        s_ = 1
+    if m_ > 19 :
+        m_ = 1
+print(count)
