93e6d441f89518c044f629650fa85a482eb81b25
jinseok-in <75413890+jinseok-in@users.noreply.github.com>
2025-04-23 23:25:07 +0900

    [Bronze II] Title: 하얀 칸, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/1100. 하얀 칸/하얀 칸.py
@@ -0,0 +1,10 @@
+def count_white_pieces() :
+    count = 0
+    for i in range(8) :
+        row = input().strip()
+        for j in range(8) :
+            if (i + j) % 2 == 0 and row[j] == 'F' :
+                count += 1
+    print(count)
+
+count_white_pieces()
